7af703bdd5fc31dd7d6b197de2a7fe51bf58b5d3
Taaha Multani <taaha.m@somaiya.edu>
2025-01-02 01:48:24 +0530

    Added coker

--- backend/test.py
+++ backend/test.py
@@ -3,7 +3,9 @@ import requests
 url = "http://127.0.0.1:8000/resume"
 data = {
     "name": "Taaha Multani",
-    "email": "taahamultanii@gmail.com"
+    "email": "taahamultanii@gmail.com",
+    "location" : {"city":"mumbai"},
+    "role" : "Admin"
 }
 
 response = requests.post(url, json=data)
